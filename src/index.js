1ae3f5a1d192cda7da528c30916d62d9ed6942ff
andrey28121989 <anenashev89@gmail.com>
2023-08-09 00:11:14 +0300

    feat: add my solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,43 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let codeArray = [];
+    let string = '';
+
+    for (let i =  0; i < expr.length; i += 10) {
+        let string = expr.slice(i, (i + 10));
+          if (string == '**********') {
+            string = ' ';
+          }
+          codeArray.push(string);
+      }
+
+    let result = [];
+
+    for (let i = 0; i < codeArray.length; i++) {
+        let word = '';
+        if (codeArray[i] == ' ') {
+          result.push(codeArray[i]);
+        }
+
+        else {
+            for (let j = 0; j < codeArray[i].length - 1; j += 2) {
+              string = codeArray[i].substr(j, 2);
+                if (string == '11') {
+                  string = '-';
+                }
+               else if (string == '10') {
+                   string = '.';
+                }
+               else {
+                 string = '';
+                }
+             word +=string;
+            }
+          result.push(MORSE_TABLE[word]);
+        }
+      }
+      return result.join('');
 }
 
 module.exports = {
